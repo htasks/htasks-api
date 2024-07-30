4fb515521a5f044eab9f9c05180ba9b0eb03a27c
blackcrw <blackzacky@protonmail.com>
2024-07-30 19:16:44 -0300

    initial commits

---
+++ src/main/java/org/htasks/htasks/HtasksApiApplication.java
@@ -0,0 +1,13 @@
+package org.htasks.htasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtasksApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HtasksApiApplication.class, args);
+    }
+
+}
